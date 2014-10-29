e1130e613a6ca3415f1ea0f39bd0f139860745b5
open-sst <jjk8932@yahoo.co.uk>
2014-10-29 07:43:59 +0000

    adapt to use oldweather imma class

--- Make_Data_Base.py
+++ Make_Data_Base.py
@@ -1,22 +1,45 @@
 import sqlite3
-from imma_classes import imma
+import imp
+
+foo = imp.load_source('IMMA.py', '..\IMMA\Python\IMMA.py')
 
 connection = sqlite3.connect('Data\ICOADS.db')
-
 cursor = connection.cursor()
 
-cursor.execute('CREATE TABLE marinereports (id text, year real, month real, day real, hour real, latitude real, longitude real, sst real, at real)')
+cursor.execute('DROP TABLE IF EXISTS marinereports')
+cursor.execute('CREATE TABLE marinereports (uid text, id text, year real, month real, day real, \
+    hour real, latitude real, longitude real, sst real, at real, country text, \
+    sst_method int, deck int, source_id int, platform_type int)')
+cursor.execute('CREATE UNIQUE INDEX IF NOT EXISTS uid ON marinereports (uid)')
 
-file = open('Data\IMMA_R2.5.1850.01.man',"r")
 
-for line in file:
-    report = imma()
-    report.read_record(line)
-    assert report.valid_imma
-    cursor.execute('INSERT INTO marinereports VALUES (?,?,?,?,?,?,?,?,?)',[report.id,report.yr,report.mo,report.dy,report.hr,report.lat,report.lon,report.sst,report.at])
+for year in range(1850,1851):
+    for month in range(1,13):
 
-file.close()
+        print year,month
+        smn = str(month)
+        syr = str(year)
+        if month < 10:
+            smn = '0'+smn
+        yrmn = syr + smn
+
+        print yrmn
+
+        file = open('Data\IMMA_R2.5.'+syr+'.'+smn+'.man',"r")
+
+        x=foo.IMMA()
+
+        test = 0
+
+        while x.read(file):
+            uid = yrmn + str(test)
+            cursor.execute('INSERT INTO marinereports VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+                   ,[uid, x.data['ID'],x.data['YR'],x.data['MO'],x.data['DY'],x.data['HR'], \
+                     x.data['LAT'],x.data['LON'],x.data['SST'],x.data['AT'],x.data['C1'], \
+                     x.data['SI'],x.data['DCK'],x.data['SID'],x.data['PT']])
+            test=test+1
+            
+        file.close()
 
 connection.commit()
-
 connection.close()
